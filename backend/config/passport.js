b17d5a5b778e30ff297a079bb3c0b6cd121aaa7f
Pablo <pablodaniel478>
2025-08-27 21:35:00 -0300

    feat:inicio de sesion con google, arreglo de espacio invisible

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -6,59 +6,59 @@ require('dotenv').config();
 const pool = require('../db'); // Importar el pool centralizado
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const userResult = await pool.query('SELECT id, nombre, email, puntos_actuales, google_id, role FROM clientes WHERE id = $1', [id]);
-        if (userResult.rows.length > 0) {
-            done(null, userResult.rows[0]);
-        } else {
-            done(new Error('User not found'), null);
-        }
-    } catch (err) {
-        done(err, null);
-    }
+    try {
+        const userResult = await pool.query('SELECT id, nombre, email, puntos_actuales, google_id, role FROM clientes WHERE id = $1', [id]);
+        if (userResult.rows.length > 0) {
+            done(null, userResult.rows[0]);
+        } else {
+            done(new Error('User not found'), null);
+        }
+    } catch (err) {
+        done(err, null);
+    }
 });
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `https://carniceria-api-vmy1.onrender.com/api/auth/google/callback`,
-    passReqToCallback: true
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `https://carniceria-api-vmy1.onrender.com/api/auth/google/callback`,
+    passReqToCallback: true
 },
 async (request, accessToken, refreshToken, profile, done) => {
-    try {
-        let user = await pool.query('SELECT id, nombre, email, puntos_actuales, google_id, role FROM clientes WHERE google_id = $1', [profile.id]);
+    try {
+        let user = await pool.query('SELECT id, nombre, email, puntos_actuales, google_id, role FROM clientes WHERE google_id = $1', [profile.id]);
 
-        if (user.rows.length === 0) {
-            const newUserQuery = `
-                INSERT INTO clientes (nombre, email, google_id, puntos_actuales, role)
-                VALUES ($1, $2, $3, $4, $5)
-                RETURNING id, nombre, email, puntos_actuales, role`;
-            const newUserValues = [
-                profile.displayName,
-                profile.emails[0].value,
-                profile.id,
-                0,
-                'user'
-            ];
-            const result = await pool.query(newUserQuery, newUserValues);
-            user = result.rows[0];
-            await pool.query(
-                'INSERT INTO preferencias_notificaciones (cliente_id) VALUES ($1)',
-                [user.id]
-            );
-        } else {
-            user = user.rows[0];
-            await pool.query('UPDATE clientes SET fecha_ultima_sesion = NOW() WHERE id = $1', [user.id]);
-        }
-        
-        done(null, user);
+        if (user.rows.length === 0) {
+            const newUserQuery = `
+                INSERT INTO clientes (nombre, email, google_id, puntos_actuales, role)
+                VALUES ($1, $2, $3, $4, $5)
+                RETURNING id, nombre, email, puntos_actuales, role`;
+            const newUserValues = [
+                profile.displayName,
+                profile.emails[0].value,
+                profile.id,
+                0,
+                'user'
+            ];
+            const result = await pool.query(newUserQuery, newUserValues);
+            user = result.rows[0];
+            await pool.query(
+                'INSERT INTO preferencias_notificaciones (cliente_id) VALUES ($1)',
+                [user.id]
+            );
+        } else {
+            user = user.rows[0];
+            await pool.query('UPDATE clientes SET fecha_ultima_sesion = NOW() WHERE id = $1', [user.id]);
+        }
+        
+        done(null, user);
 
-    } catch (err) {
-        console.error('Error durante la autenticación de Google:', err);
-        done(err, false);
-    }
+    } catch (err) {
+        console.error('Error durante la autenticación de Google:', err);
+        done(err, false);
+    }
 }));
